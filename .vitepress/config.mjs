f835e760ff62fa9e7919fbbd8c67c5816cc41161
hututusb <131585628+hututusb@users.noreply.github.com>
2024-04-21 08:09:12 +0000

    svg_push

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "ChatGLM3-Nofire模型",
   description: "A VitePress Site",
-  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  head: [["link", { rel: "icon", href: "/zhipu.svg" }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
